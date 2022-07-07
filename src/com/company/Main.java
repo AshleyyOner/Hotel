7a9542701ffc5f3317a4674ecf7a3e2eacee33cd
AshleyyOner <nihanfiliz@gmail.com>
2022-07-07 12:18:16 -0400

    java

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=0,b=1,c;
+        for (int i=0; i<10; i++) {
+            System.out.print(a+" ");
+            c=a+b;
+            a=b;
+            b=c;     }
+
+
+    }
+}
+
---
+++ src/com/company/test01.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class test01 {
+    public static void main(String[] args) {
+    }
+
+}
+
+
+
+
